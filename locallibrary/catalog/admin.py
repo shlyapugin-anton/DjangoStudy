eb8874eeb8d94f0726fee5c7b4dbbb061f87ea3d
root <root>
2019-07-01 00:42:21 -0500

    добавил шаблон для каталога, закончил урок 5 по модулю заданий

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -8,20 +8,41 @@ admin.site.register(Genre)
 # admin.site.register(BookInstance)
 admin.site.register(Language)
 
+class BookInline(admin.TabularInline):
+    model = Book
+    extra = 0
+    readonly_fields = ('title',)
+
 # Define the admin class
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BookInline]
 
 # Register the admin class with the associated model
 admin.site.register(Author, AuthorAdmin)
+
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
 
 # Register the Admin classes for Book using decorator
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
+    inlines = [BookInstanceInline]
 
 # Register the Admin classes for BookInstance using the decorator
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-    list_display = ('status', 'due_back')
+    list_display = ('status', 'due_back', 'id')
+    list_filter = ('status', 'due_back')
 
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }), 
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
